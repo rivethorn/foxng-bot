fe5240d682301a8ff0b2d8f2e5524cfe06b7cfb4
Shayan Raad <thelogicblade@gmail.com>
2026-02-18 12:08:14 -0800

    handle contact func

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 import dotenv from "dotenv";
 import { Bot } from "grammy";
 import { mainMenu } from "./keyboards";
-import { greet, testConfTxt, contactTxt } from "./messages";
+import {
+  greet,
+  testConfTxt,
+  buySubTxt,
+  buySubBtn,
+  renewSubBtn,
+} from "./messages";
 import { HandleTestAccount } from "./evaluationAcc";
 
 dotenv.config({ quiet: true });
 
+const ADMIN_ID = process.env.ADMIN_ID!;
+
 export const bot = new Bot(process.env.BOT_TOKEN!);
 
-bot.command("start", (ctx) => ctx.reply(greet, { reply_markup: mainMenu }));
+bot.command("start", async (ctx) => {
+  await ctx.reply(greet, {
+    reply_markup: mainMenu,
+  });
+});
+
+bot.on("message:contact", async (ctx) => {
+  const contact = ctx.message.contact;
+
+  await ctx.reply(greet, { reply_markup: mainMenu });
+
+  await ctx.api.sendMessage(ADMIN_ID, `the contact of ${contact.first_name}`);
+  await ctx.api.sendContact(ADMIN_ID, contact.phone_number, contact.first_name);
+});
 
 bot.on("message", async (ctx) => {
   switch (ctx.message.text) {
@@ -18,7 +39,12 @@ bot.on("message", async (ctx) => {
       await HandleTestAccount(ctx.from.id, 2);
       break;
 
-    case contactTxt:
+    case buySubBtn:
+      await ctx.reply(buySubTxt);
+      break;
+
+    case renewSubBtn:
+      // await HandleRenewAccount(ctx);
       break;
 
     default:
