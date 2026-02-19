d7eada73f0bbba2cebe4036ec1b8f4a4472b2335
Hassan Qasemi <hassanak813@outlook.com>
2026-02-19 08:23:34 +0330

    fix the renew handler

--- src/renewAcc.ts
+++ src/renewAcc.ts
@@ -1,32 +1,24 @@
 import { InlineKeyboard } from "grammy";
 import { bot } from ".";
 import { lookingForSubText, noSubFoundTxt, subFoundTxt } from "./messages";
-import { userHasAccount } from "./panelUtil";
+import { renewCache, userHasAccount } from "./panelUtil";
 
-//! THIS DOES NOT WORK
 export async function HandleRenewAccount(ctx: any) {
   const looking = await ctx.reply(lookingForSubText);
+
   const configs = await userHasAccount(ctx.from.id);
+
+  await bot.api.deleteMessage(ctx.from.id, looking.message_id);
   if (configs.length === 0) {
-    await bot.api.deleteMessage(ctx.from.id, looking.message_id);
     await ctx.reply(noSubFoundTxt);
   } else {
-    await bot.api.deleteMessage(ctx.from.id, looking.message_id);
-
     const keyboard = new InlineKeyboard();
 
-    configs.forEach(({ email, status }) => {
-      const cleaned = email.replace(/^\p{Extended_Pictographic}\s*/u, "");
-
-      console.log(cleaned);
-      keyboard.text(email, cleaned).row();
+    configs.forEach((config, idx) => {
+      keyboard.text(config.email, `renew:${idx}`).row();
     });
 
-    bot.callbackQuery("714056642", async (ctx) => {
-      if (true) {
-        await ctx.answerCallbackQuery({ text: "hello" });
-      }
-    });
+    renewCache[ctx.from.id] = configs;
 
     await ctx.reply(subFoundTxt, {
       reply_markup: keyboard,
